9c2c3cf93a73fce8395b9fcde60f2772fd537577
std84 <vizen84@gmail.com>
2020-10-07 23:18:00 +0300

    Update country.service.ts

--- src/app/_services/country.service.ts
+++ src/app/_services/country.service.ts
@@ -3,12 +3,14 @@ import { Continate } from '../_modal/continate';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Country } from '../_modal/Country';
+import { environment } from '../../environments/environment';
 @Injectable()
 export class CountryService {
+  baseUrl = environment.baseurl;
     result: Country[];
     constructor( private http: HttpClient ) { }
     getCountries(id): Observable<Country[]> {
-        return this.http.get<Country[]>('https://localhost:5001/Country/' + id);
+        return this.http.get<Country[]>(this.baseUrl + 'Country/' + id);
         // this.result = this.countries.filter(s => s.Code === id);
    
        // return of(result);
